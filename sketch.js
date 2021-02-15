07904ed1b6f58848b558807a025b03c83d2f5755
vageeshn <75984859+vageeshn@users.noreply.github.com>
2021-02-15 18:16:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var b1;
+var position,database;
+
+function setup(){
+    database = firebase.database();
+    createCanvas(500,500);
+    b1 = createSprite(250,250,10,10);
+    b1.shapeColor = "red";
+    var b1position = database.ref('ball/position');
+    b1position.on("value",readPosition,showError);
+}
+
+function draw(){
+    background("white");
+    if(position !== undefined){
+        if(keyDown(LEFT_ARROW)){
+            writePosition(-1,0);
+        }
+        else if(keyDown(RIGHT_ARROW)){
+            writePosition(1,0);
+        }
+        else if(keyDown(UP_ARROW)){
+            writePosition(0,-1);
+        }
+        else if(keyDown(DOWN_ARROW)){
+            writePosition(0,+1);
+        }
+    }
+   
+    drawSprites();
+}
+
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        'x': position.x + x,
+        'y': position.y + y
+    })
+   
+}
+function readPosition(data){
+    position = data.val();
+    b1.x=position.x;
+    b1.y=position.y;
+} 
+function showError(){
+    console.log("error in writing the database");
+}
